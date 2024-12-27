f7f088fc7ede1c7f99fb1dc709bc6eebcc70d567
KnollElias <107244530+KnollElias@users.noreply.github.com>
2024-12-27 19:35:21 +0100

    Add .inx files

---
+++ install/execute_svg_to_ifc.py
@@ -0,0 +1,8 @@
+from convert_svg_to_ifc import process_svg_layers
+import os
+
+
+svg_filename = "YOUR FILE" #replace with your filename
+output_folder = "output"
+process_svg_layers(svg_filename, output_folder)
+
